0a776510bf5e4f485681856b790efa88b0afdeef
HenrryCordovillo <henrry.cordovillo@epn.edu.ec>
2022-08-12 14:29:04 -0500

    Priemr version del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'mystore';
+  btnDisabled = true;
+  nombre: string = 'Henrry';
+  edad: number = 26;
+  imagen = 'https://source.unsplash.com/random';
+  persona = {
+    nombre: 'henrry',
+    edad: 10,
+    avatar: 'https://source.unsplash.com/random',
+    avatarTamano: 200,
+  };
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+  aumentarEdad() {
+    this.persona.edad++;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  cambiNombre(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.persona.nombre = element.value;
+  
+  }
 }

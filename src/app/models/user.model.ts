4c89eda50036c8e599b22ef735d5776313e800a4
HenrryCordovillo <henrry.cordovillo@epn.edu.ec>
2022-08-13 17:28:30 -0500

    Mas del curso

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+ export interface User{
+    id: string
+    username: string
+    imgePerfil:string
+    fullName: string
+    gender:string
+    email: string
+    age:number
+    rol: string
+    status:string
+
+}
